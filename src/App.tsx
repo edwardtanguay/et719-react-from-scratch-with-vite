104f8a03ce4efecf00065a801449e78e801f2d9d
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-01 11:00:34 +0100

    hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { FormTest } from "./components/FormTest";
 import { ImageTest } from "./components/ImageTest";
 import "./main.scss";
 
@@ -7,8 +8,20 @@ const App = () => {
 			<h1 className="text-3xl text-yellow-200 mb-3">
 				React Vite site from scratch
 			</h1>
-			<h2 className="mt-[-.7rem] mb-4 text-gray-400 italic">This site was built from scratch with Vite, based on instructions from <a target="_blank" className="underline" href="https://frontendmasters.com/courses/complete-react-v8">this course</a>.</h2>
-			<ImageTest/>
+			<h2 className="mt-[-.7rem] mb-4 text-gray-400 italic">
+				This site was built from scratch with Vite, based on
+				instructions from{" "}
+				<a
+					target="_blank"
+					className="underline"
+					href="https://frontendmasters.com/courses/complete-react-v8"
+				>
+					this course
+				</a>
+				.
+			</h2>
+			<ImageTest />
+			<FormTest />
 		</>
 	);
 };
---
+++ src/components/FormTest.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+let counter = 0;
+export const FormTest = () => {
+	const [message, setMessage] = useState('');
+	counter++;
+	return (
+		<>
+			<h3>Form Test</h3>
+			<div>Number of times component was rerendered: {counter}</div>
+			<form>
+				<input value={message} onChange={(e) => setMessage(e.target.value)} />
+			</form>
+		</>
+	);
+};
