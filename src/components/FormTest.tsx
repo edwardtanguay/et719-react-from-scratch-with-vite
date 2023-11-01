746f5e06eb0078fda0809ccfb377682fb85d49d2
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-01 11:06:12 +0100

    hooks2

--- src/components/FormTest.tsx
+++ src/components/FormTest.tsx
@@ -1,15 +1,18 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 let counter = 0;
 export const FormTest = () => {
-	const [message, setMessage] = useState('');
+	const [message, setMessage] = useState("");
 	counter++;
 	return (
 		<>
 			<h3>Form Test</h3>
 			<div>Number of times component was rerendered: {counter}</div>
 			<form>
-				<input value={message} onChange={(e) => setMessage(e.target.value)} />
+				<input
+					value={message}
+					onChange={(e) => setMessage(e.target.value)}
+				/>
 			</form>
 		</>
 	);
